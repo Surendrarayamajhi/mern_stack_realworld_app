912827ea6562f28cc6b98668cf9adb989fd76495
Surendrarayamajhi <suren.rayamajhi1425@gmail.com>
2025-12-30 01:22:50 +0545

    node module theory different b/w two file

---
+++ raw_nodejs/node_module/scriptOne.js
@@ -0,0 +1,6 @@
+(function(){
+    var a = 10;
+    console.log(a);
+})
+
+// module is nothing but separation of node file which secure from conflict b/w two same code or file write by two different developer is called iffe function
