31a30bd0f3f88660100f6ca4cd0f4498dc07eb06
Surendrarayamajhi <suren.rayamajhi1425@gmail.com>
2025-12-30 01:41:48 +0545

    how to do export and import fun or variables theory different b/w two file

---
+++ raw_nodejs/exportandimport/app.js
@@ -0,0 +1,7 @@
+let age = 20;
+
+let func1 = function(){
+    console.log("hello, this is func1 function");
+}
+// module.exports = age;
+module.exports = func1;   //direct export function also into index.js file
---
+++ raw_nodejs/exportandimport/index.js
@@ -0,0 +1,11 @@
+// let age = require("./app");
+
+
+
+// console.log(age);
+
+// another way import from another exported one 
+// const age = require("./app");
+// console.log(age);
+const func1 = require("./app");
+func1();
