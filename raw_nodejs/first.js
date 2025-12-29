08838b88b250e6e573f342f122d4860a913e3354
Surendrarayamajhi <suren.rayamajhi1425@gmail.com>
2025-12-29 15:37:34 +0545

    test another console for arthmetic operation

--- raw_nodejs/first.js
+++ raw_nodejs/first.js
@@ -1,2 +1,7 @@
 console.log("here is first console log to print message")
 // how to run nodejs file or program in code editor
+// \mern_stack_realworld_app\raw_nodejs> node first.js
+
+console.log(10+20)
+// here is first console log to print message
+// 30
