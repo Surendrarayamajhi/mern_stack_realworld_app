9d90215e4f717982aacf3acda73be0e1e0f37a7c
Surendrarayamajhi <suren.rayamajhi1425@gmail.com>
2025-12-29 15:53:29 +0545

    here print the global object

---
+++ raw_nodejs/global_obj/index.js
@@ -0,0 +1,29 @@
+// console.log(window)
+setTimeout(()=>{
+    console.log("hello setTimeOut Obj")
+},1000)
+
+// let print the global object
+console.log(global)
+/*
+<ref *1> Object [global] {
+  global: [Circular *1],
+  clearImmediate: [Function: clearImmediate],
+  setImmediate: [Function: setImmediate] {
+    [Symbol(nodejs.util.promisify.custom)]: [Getter]
+  },
+  clearInterval: [Function: clearInterval],
+  clearTimeout: [Function: clearTimeout],
+  setInterval: [Function: setInterval],
+  setTimeout: [Function: setTimeout] {
+    [Symbol(nodejs.util.promisify.custom)]: [Getter]
+  },
+  queueMicrotask: [Function: queueMicrotask],
+  structuredClone: [Getter/Setter],
+  atob: [Getter/Setter],
+  btoa: [Getter/Setter],
+  performance: [Getter/Setter],
+  fetch: [Function: fetch],
+  navigator: [Getter],
+  crypto: [Getter]
+}*/
