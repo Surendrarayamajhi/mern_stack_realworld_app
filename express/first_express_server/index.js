366b14a4d7e0dea1ee5640700f6a36f4fb91fbcd
Surendrarayamajhi <suren.rayamajhi1425@gmail.com>
2026-01-01 00:18:17 +0545

    nodemon third party package install for getting any type update without restarting server

--- express/first_express_server/index.js
+++ express/first_express_server/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req,res)=>{
-    res.send("Welcome to Home Page!");
+    res.send("Welcome to My Home Page!");
 });
 
 app.listen(8000, ()=>{
