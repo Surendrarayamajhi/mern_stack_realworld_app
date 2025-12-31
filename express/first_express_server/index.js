ef02fe1b8dc3d7103bd7dd007b789708d00c3d67
Surendrarayamajhi <suren.rayamajhi1425@gmail.com>
2026-01-01 02:08:18 +0545

    dynamic users id created in string format by using req.params

--- express/first_express_server/index.js
+++ express/first_express_server/index.js
@@ -2,7 +2,8 @@ const express = require("express");
 
 const app = express();
 
-app.use(express.json())   //express.json is builtin middleware
+// app.use(express.json())   //express.json is builtin middleware
+app.use(express.text())
 
 app.get("/", (req,res)=>{
     res.send("Welcome to My Home Page!");
@@ -12,17 +13,19 @@ app.get("/example",(req, res, next) =>{
     res.send("This is get method");
 });
 
-// app.post("/example",(req, res, next) =>{
-//     let data = req.body;
-//     console.log(data);
-//     res.send("This is an example response");
-// });
 app.post("/example",(req, res, next) =>{
-    let {name, email} = req.body;
-    console.log(name, email);  
-     
+    let data = req.body;
+    console.log(data);
     res.send("This is an example response");
 });
+
+//--this json middleware accepting api for post name and email
+// app.post("/example",(req, res, next) =>{
+//     let {name, email} = req.body;
+//     console.log(name, email);  
+
+//     res.send("This is an example response");
+// });
 
 app.put("/example",(req, res, next) =>{
     res.send("This is Put method");
@@ -35,6 +38,21 @@ app.patch("/example",(req, res, next) =>{
 app.delete("/example",(req, res, next) =>{
     res.send("This is Delete method");
 });
+
+
+// here we create some dynamic route for user by with user_id
+// first one is passing static route for users
+app.get("/users", (req, res) =>{
+    res.send("this is users");
+});
+
+// second one is passing dynamic route with the help of params
+app.get("/users/:userId", (req, res) =>{
+    const {userId} = req.params;
+    console.log(userId);
+    res.send("UserId :" + userId + " Details..");
+});
+
 app.listen(8000, ()=>{
     console.log("Server is running on port:8000");
 });
