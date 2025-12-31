6d5a3a1eb37f3e36f1ba87a2fc7ffcc843f0d777
Surendrarayamajhi <suren.rayamajhi1425@gmail.com>
2026-01-01 00:43:34 +0545

    express crud method api tested by postman

--- express/first_express_server/index.js
+++ express/first_express_server/index.js
@@ -6,6 +6,25 @@ app.get("/", (req,res)=>{
     res.send("Welcome to My Home Page!");
 });
 
+app.get("/example",(req, res, next) =>{
+    res.send("This is get method");
+});
+
+app.post("/example",(req, res, next) =>{
+    res.send("This is Post method");
+});
+
+app.put("/example",(req, res, next) =>{
+    res.send("This is Put method");
+});
+
+app.patch("/example",(req, res, next) =>{
+    res.send("This is Patch method");
+});
+
+app.delete("/example",(req, res, next) =>{
+    res.send("This is Delete method");
+});
 app.listen(8000, ()=>{
     console.log("Server is running on port:8000");
 });
